317a53fec07fc9a52634204b6c712236b67377d0
vedadr <vedadr@gmail.com>
2020-02-28 12:33:39 +0100

    latest changes in the scripts

--- generate_xml_and_other_examples/format_fix_example_csv_input.py
+++ generate_xml_and_other_examples/format_fix_example_csv_input.py
@@ -4,6 +4,8 @@ It can be useful when working on new ACS project to make sure its
 variables has the same properties as the old one.
 
 """
+import json
+
 from lxml import etree as et
 from os.path import join
 import pandas as pd
@@ -12,62 +14,121 @@ import pandas as pd
 def main(settings, file_to_fix):
     """
     Function to run them all
+    :param settings:
     :param template_file: file to use as a template
     :param file_to_fix: file to be checked and fixed
     :return:
     """
     # what do you want to fix? Note that index is matching indexes in form_template variable.
     # fix_scope = {
-    #     'indent': 0,
-    #     'dataType': 1,
-    #     'dataTypeLength': 2,
-    #     'formatting': 3,
-    #     'aggMethod': 4,
-    #     'AggregationStr': 5,
-    #     'customFormatStr': 6,
-    #     'suppType': 7,
-    #     'SuppField': 8,
-    #     'suppFlags': 9,
-    #     'BubbleSizeHint': 10
-    # }
+    #     'aggMethod': 'agg_method',
+    #     'AggregationStr': 'agg_string',
+    #     'BubbleSizeHint': 'bubble_size',
+    #     'FR': 11,
+    #     'PN': 12
+    #     }
 
-    fix_scope = {
-        'aggMethod': 4,
-        'AggregationStr': 5,
-        'BubbleSizeHint': 10,
-        'FR': 11,
-        'PN': 12
-    }
+    """
+       scope = {'PT':'Sequential',
+        'PI':'false',
+        'PN':'blues',
+        'BracketSourceVarGUID':'',
+        'BracketFromVal':'0',
+        'BracketToVal':'0',
+        'BracketType':'None',
+        'FirstInBracketSet':'false',
+        'notes':'',
+        'PrivateNotes':'',
+        'name':'T002_001',
+        'label':'PoblaciónTotal',
+        'qLabel':'PoblaciónTotal',
+        'indent':'0',
+        'dataType':'4',
+        'dataTypeLength':'0',
+        'formatting':'9',
+        'customFormatStr':'',
+        'suppType':'0',
+        'SuppField':'', 
+        'suppFlags':'',
+        'aggMethod':'1',
+        'DocLinksAsString':'',
+        'FR':'',
+        'AggregationStr':'Add',
+        'BubbleSizeHint':'2'}
+    """
+    #
+    # fix_scope = {
+    #     'label':'PoblaciónTotal',
+    #     'qLabel':'PoblaciónTotal',
+    #     'PN':'blues',
+    #     'FR':'',
+    #     'AggregationStr':'Add',
+    # }
 
     parser = et.XMLParser(strip_cdata=False)
     doc_to_fix = et.parse(file_to_fix, parser=parser)
     variables = doc_to_fix.xpath('//SurveyDataset[@abbreviation="ORG"]//variable')
     for var in variables:
-        try:
-            var_values = settings[var.attrib['name'][-3:]]
-        except KeyError:
-            continue
 
-        for fix, index in fix_scope.items():
-            var.attrib[fix] = var_values[index]
+        variable_name_from_full = var.attrib['name'][[i for i,c in enumerate(var.attrib['name']) if c == '_'][-2]+1:]
+
+        var_values = settings[settings['name'].str.lower() == variable_name_from_full.lower()]
+
+        for attribute in settings.columns:
+            if attribute == 'name':  # we don't want to change the name
+                continue
+            try:
+                var.attrib[attribute]
+            except KeyError:
+                continue
+            try:
+                var.attrib[attribute] = var_values[attribute].item()
+            except ValueError:
+                print('stop')
+
     print('Writing doc ...')
     doc_to_fix.write(file_to_fix, pretty_print=True)
 
 
+def get_palettes():
+
+    with open(r'C:\Projects\dingo-maps\maps\color-palettes\ced-color-palettes.json', 'r') as file:
+        palette_definitions = json.load(file)
+    return {tag['title']: tag['id'] for tag in palette_definitions}
+
 if __name__ == '__main__':
 
-    projects = {'PC2003.xml', 'PC2004.xml', 'PC2005.xml', 'PC2006.xml', 'PC2006.xml',
-                'PC2007.xml', 'PC2008.xml', 'PC2009.xml', 'PC2010.xml', 'PC2011.xml',
-                'PC2012.xml', 'PC2013.xml', 'PC2014.xml', 'PC2015.xml', 'PC2016.xml',
-                'PC2017.xml'}
+    # projects = {'CA2009.xml', 'PC2018.xml',
+    #             'NOMEN2011.xml', 'ELEC2011.xml',
+    #             'SSOC2011.xml', 'TCN2011.xml',
+    #             'nac2011.xml', 'renta2016.xml',
+    #             'EVR2011.xml', 'DEF2011.xml',
+    #             'PERE2011.xml', 'SHP2011.xml',
+    #             'VEH2014.xml', 'mat2011.xml',
+    #             'CON2011.xml', 'CATAS2012.xml'}
 
     working_dir = r'C:\Projects\Website-ASP.NET\pub\ReportData\Metadata'
 
-    input_file = pd.read_csv('settings.csv')
+    palettes = get_palettes()
+    input_file_variables = pd.read_csv('settings.csv')
+    map_type_mapping = {'bubbles': 'Add', 'Shaded': 'Rate|||100000',
+                        'Bubbles': 'Add', 'shaded': 'Rate|||100000'}
 
+    column_name_mapping = {'variable': 'name',
+                           'name_variable': 'label',
+                           'short_name': 'qlabel',
+                           'Color palette': 'PN',
+                           'Cutpoints': 'FR',
+                           'Type of map': 'AggregationStr'}
+
+    input_file_variables.rename(column_name_mapping, axis=1, inplace=True)
+    input_file_variables.AggregationStr.replace(map_type_mapping, inplace=True)
+    input_file_variables.PN = input_file_variables.PN.str.title().replace(palettes)
+
+    projects = input_file_variables.Project.drop_duplicates()
     for project in projects:
         print(f'Editing {project}')
 
-        main(input_file, join(working_dir, project))
+        main(input_file_variables, join(working_dir, project + '.xml'))
 
         print('Done!')
