4e3c1c37d31e4f4c0398650fbbb6bd8f95b92e95
turusan79 <51401420+turusan79@users.noreply.github.com>
2021-04-20 10:20:09 +0200

    ELECHP1898

--- generate_xml_and_other_examples/fix_spanish_characters_ELECHP.py
+++ generate_xml_and_other_examples/fix_spanish_characters_ELECHP.py
@@ -27,7 +27,7 @@ def main():
         project_list = [project.name for project in path.iterdir() if project.name.endswith('.xml')]
     else:
         # in case you don't want all projects to be checked, you can create a list of selected projects for fixing
-        project_list = {'ELECHP1901.xml','ELECHP1907.xml'}
+        project_list = {'ELECHP1898.xml','ELECHP1907.xml'}
 
     chars_to_replace = {'Ã³': 'ó', 'Ã¡': 'á', 'Ã±': 'ñ', 'Ãº': 'ú', 'Ã©': 'é'}
 
